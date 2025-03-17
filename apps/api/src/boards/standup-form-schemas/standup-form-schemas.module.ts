8f0acf0cd8f58f0233c0b92c6814e3433e65a044
Hyukoo Alex Kwon <hyuk.kwon.epic@outlook.com>
2025-03-17 09:31:11 +0900

    STNDP-15 Server error after a long connection (#6)

--- apps/api/src/boards/standup-form-schemas/standup-form-schemas.module.ts
+++ apps/api/src/boards/standup-form-schemas/standup-form-schemas.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StandupFormSchemasService } from './standup-form-schemas.service';
 import { StandupFormSchemasController } from './standup-form-schemas.controller';
-
+import { DbModule } from 'src/db/db.module';
 @Module({
   controllers: [StandupFormSchemasController],
   providers: [StandupFormSchemasService],
+  imports: [DbModule],
 })
 export class StandupFormSchemasModule {}
